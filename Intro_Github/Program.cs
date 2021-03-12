ede47577d349ab5744dd3d5d4d71f8a9b7262875
Boris8008 <00024020@uca.edu.sv>
2021-03-11 20:53:29 -0600

    Add Program

---
+++ Intro_Github/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Intro_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+
+            Console.Write("Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\nEnter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nAdding {a} + {b} equals {Addnumbers(a,b)}");
+        }
+
+        public static int Addnumbers(int a, int b)
+        {
+
+            return a + b;
+        }
+    }
+}
